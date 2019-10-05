973cd5f2b096aaa9fb80753171d4d8f5e48f82a0
W and L <34315237+WandLZhang@users.noreply.github.com>
2019-10-04 21:05:49 -0400

    ASM beta

--- common/install-tools.sh
+++ common/install-tools.sh
@@ -18,7 +18,7 @@
 export PROJECT=$(gcloud config get-value project)
 export WORK_DIR=${WORK_DIR:="${PWD}/workdir"}
 
-export ISTIO_VERSION=1.1.4
+export ISTIO_VERSION=1.1.15
 
 ## Install Tools
 mkdir -p $WORK_DIR/bin
@@ -32,23 +32,6 @@ curl -sLO https://raw.githubusercontent.com/ahmetb/kubectx/master/kubectx
 chmod +x kubectx 
 mv kubectx $WORK_DIR/bin
 
-
-# Install Helm
-#curl https://raw.githubusercontent.com/kubernetes/helm/master/scripts/get > get_helm.sh
-#chmod 700 get_helm.sh
-#./get_helm.sh &> /dev/null
-#cp /usr/local/bin/helm $WORK_DIR/bin
-#rm ./get_helm.sh
-
-HELM_VERSION=v2.13.0
-wget -q https://storage.googleapis.com/kubernetes-helm/helm-"$HELM_VERSION"-linux-amd64.tar.gz
-tar -xvzf helm-"$HELM_VERSION"-linux-amd64.tar.gz
-mv linux-amd64/helm $WORK_DIR/bin
-mv linux-amd64/tiller $WORK_DIR/bin
-rm helm-"$HELM_VERSION"-linux-amd64.tar.gz
-rm -rf linux-amd64
-
-
 # Download Istio
 curl -L https://git.io/getLatestIstio | ISTIO_VERSION=$ISTIO_VERSION sh -
 cp istio-$ISTIO_VERSION/bin/istioctl $WORK_DIR/bin/.
@@ -58,7 +41,6 @@ mv istio-$ISTIO_VERSION $WORK_DIR/
 #curl -o yq.v2 -OL https://github.com/mikefarah/yq/releases/download/2.3.0/yq_linux_amd64
 #chmod +x yq.v2
 #mv yq.v2 $WORK_DIR/bin
-
 
 
 
